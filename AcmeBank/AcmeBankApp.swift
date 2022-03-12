36993a758fefae80f66578e1c174be20a5e9ef6a
Kir <babkin.kir@gmail.com>
2022-03-12 18:48:00 -0500

    Initial Commit

---
+++ AcmeBank/AcmeBankApp.swift
@@ -0,0 +1,17 @@
+//
+//  AcmeBankApp.swift
+//  AcmeBank
+//
+//  Created by Kirill Babkin on 3/12/22.
+//
+
+import SwiftUI
+
+@main
+struct AcmeBankApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
